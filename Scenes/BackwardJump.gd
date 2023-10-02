9c32d6c7ae120e5c4ad30a766975e8dbe0445268
sudzoof <60050712+sudzoof@users.noreply.github.com>
2023-10-02 18:46:37 -0400

    Fix directional jump landing
    You now cancel to idle upon landing after a Jump. This is temporary and should be baked into the player body physics eventually.

--- Scenes/BackwardJump.gd
+++ Scenes/BackwardJump.gd
@@ -3,3 +3,8 @@ extends Move
 func _on_start():
 	player.set_cancels([])
 	player.velocity = Vector2(-player.SPEED, -player.JUMP)
+
+# TODO change this to universal behavior
+func _on_tick():
+	if player.is_on_floor() and frame > 3:
+		controller.change_move("Idle")
